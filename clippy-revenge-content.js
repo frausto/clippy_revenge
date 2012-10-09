360efa2eb7e31fd03913ebb14f783e3ea7ee1881
Nolan Frausto <nrfrausto@gmail.com>
2012-10-09 16:41:33 +0200

    get clippy to appear

---
+++ clippy-revenge-content.js
@@ -0,0 +1,5 @@
+clippy.load('Clippy', function(agent){
+    // do anything with the loaded agent
+    agent.show();
+    agent.animate();
+});
--- clippy-revenge.js
+++ clippy-revenge.js
@@ -1,15 +1,9 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var min = 1;
-var max = 2;
-var current = min;
+var current = "inactive"
 
 function updateIcon() {
-  chrome.browserAction.setIcon({path:"icon" + current + ".png"});
-  current++;
-  if (current > max){current = min;}
+  if (current == "inactive"){ current = "active";}else{current = "inactive";}
+
+  chrome.browserAction.setIcon({path:"icon-" + current + ".png"});
 }
 
 chrome.browserAction.onClicked.addListener(updateIcon);
